0582fd4f65e7df7a4e4c186e2964b1fec3184db6
erdemir123 <sadikerdemir93@gmail.com>
2023-01-12 22:57:09 +0300

    project created

---
+++ app/[...pokemon]/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = ({params}) => {
+  console.log(params.pokemon[0]);
+  return (
+    <div>
+      s
+    </div>
+  )
+}
+
+export default page
---
+++ components/Form.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import Styles from "./form.module.css"
+
+async function getCountry() {
+  let res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
+  return res.json();
+}
+export default async function Home() {
+  let data = await getCountry();
+  return (
+    <div className={Styles.container} >
+      {data?.results.map((item, index) => (
+        <Link href={`/pokemon/${item.name}`} key={index} className={Styles.card}>
+          <Image  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`} alt={item.name} width={200} height={200} />
+          <div className={Styles.cardTitle}>{item?.name}</div>
+        </Link>
+        
+      ))}
+    </div>
+  );
+}
+
+
